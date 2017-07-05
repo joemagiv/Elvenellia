e962e60550e461e2977f6ca7dce809e8844b35d1
Joseph Magdalena <joemagiv@gmail.com>
2017-07-04 20:33:30 -0700

    Initial commit.

---
+++ Assets/GameController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+	public int currentPopulation;
+	public double currentMagic;
+	public float currentMagicGrowthRate;
+	public float prayerStrengthRate;
+	public float populationGrowthRate;
+	public float growthTimer;
+
+	public Text currentMagicText;
+	public Text currentMagicGrowthRateText;
+	public Text currentPopulationText;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public void Pray (){
+		currentMagic += (currentMagicGrowthRate * prayerStrengthRate);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		currentMagic += currentMagicGrowthRate;
+		Debug.Log ("Current magic is: " + currentPopulation);
+		currentMagicText.text = "Magic: " + currentMagic.ToString ("0.##");
+		currentMagicGrowthRateText.text = "Magic Growth Rate: " + currentMagicGrowthRate.ToString ("0.###");
+		currentPopulationText.text = "Population: " + currentPopulation.ToString (); 
+
+		//Tracking population growth
+		growthTimer++;
+		if (growthTimer > populationGrowthRate){
+			currentPopulation++;
+			growthTimer = 0;
+		}
+	}
+}
